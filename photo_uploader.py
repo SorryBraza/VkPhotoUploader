dcb58dff823e4fe2b5273d3c6e05ae795fc9b21b
Dorzh Dorzhiev <ddr.03@ya.ru>
2023-08-08 18:15:25 +0300

    Changed 2 commit

--- photo_uploader.py
+++ photo_uploader.py
@@ -86,12 +86,9 @@ def find_max_size(dict_sizes):
 
 
 if __name__ == '__main__':
-    access_token = 'vk1.a.aDilNhpoUhffd6M4Wco96-lMvxDA16Y7heq7ksWRg0x4WBO3LZAWmKqBL4HO-llSaS9ZI8k0tG7qN7lqX6aMWI-wU2hdEBe7DjlloaxigjWRxP9oehtanrkWoGK_LMV1mAU7GIFAYoU1pdeGkOWu-ecJY_KVLzIOtjbbCuQcaQm6fWZkaDPcz7Yllum8mIs1_sIXl8tDT90fa5kRm1EPew'
-    user_id = 201669985
-    ya_token = 'y0_AgAAAAA0KJauAADLWwAAAADkDekeZiFNSEeQQIqqtfBmstbZMlOkJt8'
-    # access_token = input('Введите токен ВК: ') 
-    # user_id = input('Введите ID пользователя: ') 
-    # ya_token = input('Введите токен Я.Диск: ') 
+    access_token = input('Введите токен ВК: ') 
+    user_id = input('Введите ID пользователя: ') 
+    ya_token = input('Введите токен Я.Диск: ') 
     vk = VK(access_token, user_id)
     ya_disk = YaDisck(ya_token)
     folder_name = ya_disk.create_folder_yadisck('vk')
